611a7a4f78874c4e4565b63cf0269f9da0b8f559
rcaoagdan <rcaoagdan@icloud.com>
2021-04-14 12:49:31 -0700

    new

---
+++ Challenge01/helloworld.py
@@ -0,0 +1,7 @@
+# Code Name: Hello World
+# Author : Raymonc (ray) Caoagdan
+# Date of Last revision : 04/14/2021
+# Purpose: Install Python Plug in and execute greatest command print "Hello World"
+
+msg = "Hello World"
+print(msg)
