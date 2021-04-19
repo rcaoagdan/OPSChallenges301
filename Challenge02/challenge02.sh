c1c8a2f9865c1a5b0c1fcd57d497f52aabb8f9e6
rcaoagdan <rcaoagdan@icloud.com>
2021-04-18 20:30:51 -0700

    new

--- Challenge02/challenge02.sh
+++ Challenge02/challenge02.sh
@@ -5,3 +5,16 @@
 #Date of Last Revision: 04/18/2021
 #Purpose: Arrays : Append; Date&Time 
 
+#variables
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+hour=$(date +%H)
+minute=$(date +%M)
+second=$(date +%S)
+
+#function
+date_and_time(){
+    echo "The Current date:" $day-$month-$year
+}
+date_and_time
