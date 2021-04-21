8194be8407543518457be8900dab54e43d5c4f0a
rcaoagdan <rcaoagdan@icloud.com>
2021-04-20 22:13:00 -0700

    update

--- Challenge02/challenge02.sh
+++ Challenge02/challenge02.sh
@@ -3,7 +3,7 @@
 #Script: Ops Challenge 02 
 #Author: Ray Caoagdan
 #Date of Last Revision: 04/18/2021
-#Purpose: Arrays : Append; Date&Time 
+#Purpose:  Append; Date&Time 
 
 ##############################################################################
 # Variabeles used 
