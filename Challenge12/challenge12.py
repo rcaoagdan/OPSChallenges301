488f99d1d91757d13fcd3b7dc9ead23ff3066165
rcaoagdan <rcaoagdan@icloud.com>
2021-05-24 17:40:00 -0700

    new

---
+++ Challenge12/challenge12.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#Script: Ops Challenge 12
+#Author: Ray Caoagdan
+#Date of Last Revision: 05/19/2021
+#Purpose: Python Psutil
+
+##############################################################################
+# import psutil
+##############################################################################
+import psutil 
+
+##############################################################################
+#Main
+##############################################################################
+print('Hello Welcome! \n')
+print('user: Time Spent in User mode')
+print('nice: Time Spent in User mode low priority')
+print('sytem: Time Spent in System Mode')
+print('idle: Time Spent in idle tasks')
+print('iowait: Time Spent waiting for I/O to comeplte')
+print('irq: Time Spent Hardware interrupts')
+print('softirq: To Spent Software Interrupts')
+print('steal: Time spent bu other O/S running in a virtuialized enviorment \n')
+print('guest_nice: Time Spent running a virtual CPU for guest OS under control of Linux Kernel')
+print(psutil.cpu_times(), '\n')
+
+##############################################################################
+#End
+##############################################################################
+
